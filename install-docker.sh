feb6eb2d7ae9b93ccba4376b0442d646570a9f60
Brennen Raimer <brennen@reshiram.fios-router.home>
2019-03-03 14:07:14 -0500

    a few tweaks to fix problems I found

--- install-docker.sh
+++ install-docker.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update && sudo apt-get -y dist-upgrade
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=armhf] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update && sudo apt-get -y install docker-ce
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && \
+sudo apt-get update && sudo apt-get -y install docker-ce && sudo usermod -a -G docker $USER
 sudo apt-get install -y python-pip
 sudo pip install docker-compose
